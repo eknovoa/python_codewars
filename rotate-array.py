b58ebf1438e996fc7d04d05dcc277f0562ab43cc
Erin Novoa <68754679+eknovoa@users.noreply.github.com>
2022-06-10 13:24:48 -0500

    Create rotate-array.py

---
+++ rotate-array.py
@@ -0,0 +1,52 @@
+"""
+Create a method named "rotate" that returns a given array with the elements inside the array rotated n spaces.
+
+If n is greater than 0 it should rotate the array to the right. If n is less than 0 it should rotate the array to the left. If n is 0, then it should return the array unchanged.
+
+Example:
+
+with array [1, 2, 3, 4, 5]
+
+n = 1      =>    [5, 1, 2, 3, 4]
+n = 2      =>    [4, 5, 1, 2, 3]
+n = 3      =>    [3, 4, 5, 1, 2]
+n = 4      =>    [2, 3, 4, 5, 1]
+n = 5      =>    [1, 2, 3, 4, 5]
+n = 0      =>    [1, 2, 3, 4, 5]
+n = -1     =>    [2, 3, 4, 5, 1]
+n = -2     =>    [3, 4, 5, 1, 2]
+n = -3     =>    [4, 5, 1, 2, 3]
+n = -4     =>    [5, 1, 2, 3, 4]
+n = -5     =>    [1, 2, 3, 4, 5]
+The rotation shouldn't be limited by the indices available in the array. Meaning that if we exceed the indices of the array it keeps rotating.
+
+Example:
+
+with array [1, 2, 3, 4, 5]
+
+n = 7        =>    [4, 5, 1, 2, 3]
+n = 11       =>    [5, 1, 2, 3, 4]
+n = 12478    =>    [3, 4, 5, 1, 2]
+
+"""
+
+def rotate(data, n):
+    if len(data) == 0:
+        return data
+    elif n == 0:
+        return data
+    elif n < 0:
+        j = -1
+        while j >= n:
+            val = data[0]
+            data.remove(data[0])
+            data.append(val)
+            j-=1
+    i = 1
+    while i <= n:
+        value = data.pop()
+        data.insert(0,value)
+        i+=1
+    return data
+  
+ 
